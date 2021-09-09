7475a52faf5f97f5109e1ac09ad363e12259f7df
purplesheep513 <prune1231@gmail.com>
2021-09-09 23:14:27 +0900

    시간초과가 났다..왜일까

--- practice03.py
+++ practice03.py
@@ -1,28 +1,31 @@
 import sys
 T = int(sys.stdin.readline())
-N = int(sys.stdin.readline())
-graph = list(map(int, sys.stdin.readline().split()))
-graph.insert(0,0)
-for i in graph:
-    if i == graph[i]:
-        graph[i] = 0
-def dfs(n) :
-    visit = [ 0 for _ in range(len(graph))]
-    visit.insert(0,-1)
-    stack = []
-    visit[n] = 1
-    stack.append(n)
+for _ in range(T):
+    N = int(sys.stdin.readline())
+    answer = set()
+    graph = list(map(int, sys.stdin.readline().split()))
+    graph.insert(0,0)
 
-    while stack:
-        N = stack.pop()
-        if visit[graph[N]] == 0:
-            visit[graph[N]] = 1
-            stack.append(graph[N])
-        elif visit[graph[N]] == -1 :
-            print('no cycle')
-            print(visit)
-        else :
-            print('cycle')
-            print(visit)
+    def dfs(n) :
+        visit = [ 0 for _ in range(len(graph))]
+        visit.insert(0,-1)
+        stack = []
+        visit[n] = 1
+        stack.append(n)
 
-dfs(1)
+        while stack:
+            n = stack.pop()
+            if n == graph[n]:#사이클o
+                answer.add(n)
+            elif visit[graph[n]] == 0:#방문을 아직 안 한 것
+                visit[graph[n]] = 1
+                stack.append(graph[n])
+            elif visit[graph[n]] == -1 : #사이클x
+                continue 
+            else :#사이클o
+                answer.add(n)
+
+    for i in range(1,N+1):
+        dfs(i)
+
+    print(len(graph)-len(answer)-1)
