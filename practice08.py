937335010c40c0810314166a72c81d1826e538bc
purplesheep513 <prune1231@gmail.com>
2021-09-18 18:59:57 +0900

    벽으로 막혀있는 경우에대해 다시 생각해야할듯

--- practice08.py
+++ practice08.py
@@ -1,5 +1,6 @@
 import sys
 from collections import deque
+import copy
 
 M,N = map(int,sys.stdin.readline().rstrip().split())
 
@@ -7,15 +8,16 @@ graph = [[] for _ in range(N)]
 for i in range(N):
     graph[i] = list(map(int,sys.stdin.readline().rstrip().split()))
 
+graph2 = copy.deepcopy(graph)
 visit = [[0 for _ in range(M)] for _ in range(N)]
-
+visit2 = [[0 for _ in range(M)] for _ in range(N)]
 dx = [-1,0,1,0]
 dy = [0,-1,0,1]
 
 def bfs(x,y):
     visit[x][y]=1
     queue = deque([(x,y)])
-    if graph[x][y] == 1 and visit[x][y] == 0:
+    if graph[x][y] != 0:
         queue.popleft()
     while queue :
         x,y = queue.popleft()
@@ -29,11 +31,14 @@ def bfs(x,y):
                         queue.append((nx,ny))
 
 def ripe(x,y):
+    visit2[x][y] = 1
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0<= nx < N and 0 <= ny < M and graph[x][y]==1:
-            graph[nx][ny] = 1
+        if 0<= nx < N and 0 <= ny < M :
+            if visit2[nx][ny] == 0 :
+                if graph[x][y]==1 and graph[nx][ny] ==0:
+                    graph2[nx][ny] = 1
 
 cnt = 0
 day = 0
@@ -45,11 +50,14 @@ while True:
             if visit[i][j] == 0 and graph[i][j] ==0 :
                 bfs(i,j)
                 cnt+=1
+    if cnt != 0 :
         for i in range(N):
             for j in range(M):
                 ripe(i,j)
                 cnt = 0
         day += 1
+        graph = copy.deepcopy(graph2)
     else: break
+
 
 print(day)
